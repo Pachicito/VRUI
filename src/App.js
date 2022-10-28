1ac52e0a823054431877e8dd5dd94e2fd0d1cc7a
Joe Kane <jkd404@gmail.com>
2022-10-28 12:10:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class LambdaDemo extends Component {
     this.setState({ loading: true })
     fetch('https://api.figma.com/v1/files/fkEZhLwbfw47dSnibASSFE?depth=2', {
       headers: {'X-FIGMA-TOKEN': 'figd_xQfn3nIwieZctQtIvmHNP8lMCAVvQ0EtGDR4Zu_y'}})
-      .then(response => JSON.parse(response.text()))
-      .then(response => this.setState({ loading: false, msg: response.document }))
+      .then(response => response.text())
+      .then(json => this.setState({ loading: false, msg: json }))
   }
 
   render() {
